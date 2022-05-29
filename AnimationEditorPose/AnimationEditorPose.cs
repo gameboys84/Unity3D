1e7649749e8d5a77d8b845b3f57f25d865480b75
gameboys84 <gameboys84@163.com>
2022-05-29 14:18:59 +0800

    add some interesting files written by Erik Ross

---
+++ AnimationEditorPose/AnimationEditorPose.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class AnimationEditorPose : MonoBehaviour {
+	public Animator Animator;
+	public AnimationClip Clip;
+
+	[Range(0, 1)]
+	public Single Time;
+
+	void Update() {
+		if (Application.isPlaying) {
+			Destroy(this);
+			return;
+		}
+
+#if UNITY_EDITOR
+    if (Clip) {
+      // try to grab animator
+      if (!Animator) {
+        Animator = gameObject.GetComponentInParent<Animator>();
+
+        if (!Animator) {
+          Animator = gameObject.GetComponentInChildren<Animator>();
+
+          if (!Animator) {
+            return;
+          }
+        }
+      }
+
+      Time = Mathf.Clamp01(Time);
+      Clip.SampleAnimation(Animator.gameObject, Clip.length * Time);
+    }
+#endif
+	}
+}
--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Unity3D
 Snappy在C#下的实现
 
 同时解决引入Unity工程后，在安卓环境下，使用IL2CPP会崩溃的问题
+
+## 外部工程
+以下目录来自作者`Erik Ross`开发: ``AnimationEditorPose, DeterministicRNG, MeshCombineAndOccluder, Outlines, PerformanHealthBars, SfxPool, Singleton``
+来源: https://github.com/fholm/unityassets.git
+旧版本记录: https://github.com/fholm/unityassets/tree/old
+
+
+
+
